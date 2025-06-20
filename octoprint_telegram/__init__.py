jacopotediosi/OctoPrint-Telegram

commit 3ca33bec4dba5e8c1ac1216855581e070c061b16

Refactor of is_command_allowed
Such a critical function was not clearly understandable. Now it is.

octoprint_telegram/__init__.py
@@ -2047,26 +2047,36 @@ class TelegramPlugin(
     def str2bool(self, v):
         return v.lower() in ("yes", "true", "t", "1")
 
-    # Checks if the received command is allowed to execute by the user
+    # Check if the received command is allowed to be executed by the user
     def is_command_allowed(self, chat_id, from_id, command):
+        # If no commands, nothing to allow
+        if not command:
+            return False
+
+        is_group_chat = int(chat_id) < 0
+
+        chat_settings = self.chats.get(chat_id, {})
+        chat_accept_commands = chat_settings.get("accept_commands", False)
+        chat_accept_this_command = chat_settings.get("commands", {}).get(command, False)
+        chat_allow_commands_from_users = chat_settings.get("allow_users", False)
+
+        from_settings = self.chats.get(from_id, {})
+        from_accept_commands = from_settings.get("accept_commands", False)
+        from_accept_this_command = from_settings.get("commands", {}).get(command, False)
+
+        # Always allowed commands (e.g., /help, etc)
         if "bind_none" in self.tcmd.commandDict[command]:
             return True
-        if command is not None or command != "":
-            if self.chats[chat_id]["accept_commands"]:
-                if self.chats[chat_id]["commands"][command]:
-                    return True
-                elif int(chat_id) < 0 and self.chats[chat_id]["allow_users"]:
-                    if (
-                        self.chats[from_id]["commands"][command]
-                        and self.chats[from_id]["accept_commands"]
-                    ):
-                        return True
-            elif int(chat_id) < 0 and self.chats[chat_id]["allow_users"]:
-                if (
-                    self.chats[from_id]["commands"][command]
-                    and self.chats[from_id]["accept_commands"]
-                ):
-                    return True
+
+        # Commands allowed for all chat members (both in private chat and in groups)
+        if chat_accept_commands and chat_accept_this_command:
+            return True
+
+        # User personal permissions within groups
+        if is_group_chat and chat_allow_commands_from_users:
+            if from_accept_commands and from_accept_this_command:
+                return True
+
         return False
 
     # Helper function to handle /editMessageText Telegram API commands

commit 83088bae8477c66096a409c8152b192aac7571ab

Fix empty group title in plugin settings
The title for groups was blank in the "Known Chats" section of the plugin settings. This was due to an indentation error introduced by commit 3ca33bec4dba5e8c1ac1216855581e070c061b16.

octoprint_telegram/__init__.py
@@ -451,14 +451,14 @@ class TelegramListener(threading.Thread):
         elif chat["type"] == "private":
             data["private"] = True
 
-        title_parts = []
-        if "first_name" in chat:
-            title_parts.append(chat["first_name"])
-        if "last_name" in chat:
-            title_parts.append(chat["last_name"])
-        if "username" in chat:
-            title_parts.append(f"@{chat['username']}")
-        data["title"] = " - ".join(title_parts)
+            title_parts = []
+            if "first_name" in chat:
+                title_parts.append(chat["first_name"])
+            if "last_name" in chat:
+                title_parts.append(chat["last_name"])
+            if "username" in chat:
+                title_parts.append(f"@{chat['username']}")
+            data["title"] = " - ".join(title_parts)
 
         from_id = chat_id
         # If message is from a group, chat_id will be left as id of group
@@ -1005,7 +1005,7 @@ class TelegramPlugin(
                     if not json["ok"]:
                         raise Exception("invalid request")
                     chat = json["result"]["chat"]
-                    if chat["type"] == "group":
+                    if chat["type"] == "group" or chat["type"] == "supergroup":
                         data["private"] = False
                         data["title"] = chat["title"]
                     elif chat["type"] == "private":