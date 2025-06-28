af5c04c99f4a0b8d018bf3d0943e6465aa995efe
Jacopo Tediosi <jacopotediosi@gmail.com>
2025-06-28 02:17:19 +0200

    Escape entities in formatted variables

--- octoprint_telegram/telegram_utils.py
+++ octoprint_telegram/telegram_utils.py
@@ -1,5 +1,7 @@
 import logging
+import re
 import traceback
+from typing import Optional
 
 import requests
 
@@ -80,3 +82,39 @@ class TelegramUtils:
 
 def is_group_or_channel(chat_id):
     return int(chat_id) < 0
+
+
+def escape_markdown(text: str, version: int = 1, entity_type: Optional[str] = None) -> str:
+    """
+    Copied from python-telegram-bot/python-telegram-bot
+
+    Helper function to escape telegram markup symbols.
+
+    .. versionchanged:: 20.3
+        Custom emoji entity escaping is now supported.
+
+    Args:
+        text (:obj:`str`): The text.
+        version (:obj:`int` | :obj:`str`): Use to specify the version of telegrams Markdown.
+            Either ``1`` or ``2``. Defaults to ``1``.
+        entity_type (:obj:`str`, optional): For the entity types
+            :tg-const:`telegram.MessageEntity.PRE`, :tg-const:`telegram.MessageEntity.CODE` and
+            the link part of :tg-const:`telegram.MessageEntity.TEXT_LINK` and
+            :tg-const:`telegram.MessageEntity.CUSTOM_EMOJI`, only certain characters need to be
+            escaped in :tg-const:`telegram.constants.ParseMode.MARKDOWN_V2`. See the `official API
+            documentation <https://core.telegram.org/bots/api#formatting-options>`_ for details.
+            Only valid in combination with ``version=2``, will be ignored else.
+    """
+    if int(version) == 1:
+        escape_chars = r"_*`["
+    elif int(version) == 2:
+        if entity_type in ["pre", "code"]:
+            escape_chars = r"\`"
+        elif entity_type in ["text_link", "custom_emoji"]:
+            escape_chars = r"\)"
+        else:
+            escape_chars = r"\_*[]()~`>#+-=|{}.!"
+    else:
+        raise ValueError("Markdown version must be either 1 or 2!")
+
+    return re.sub(f"([{re.escape(escape_chars)}])", r"\\\1", text)
