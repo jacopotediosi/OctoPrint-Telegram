7ba16299d1a9a09a21f52e3640c8e09602ab37bd
Jacopo Tediosi <jacopotediosi@gmail.com>
2026-09-06 21:57:06 +0200

    Fix /togglepause not working on OctoPrint 2.x

--- octoprint_telegram/commands/cmd_togglepause.py
+++ octoprint_telegram/commands/cmd_togglepause.py
@@ -9,12 +9,12 @@ render_emojis = Emoji.render_emojis
 class CmdTogglePause(BaseCommand):
     @override
     def execute(self, command_context: CommandContext) -> None:
-        if self.plugin_context.printer.is_printing():
-            msg = render_emojis("{emo:pause} Pausing the print.")
-            self.plugin_context.printer.pause_print(user=command_context.user)
-        elif self.plugin_context.printer.is_paused():
+        if self.plugin_context.printer.is_paused():
             msg = render_emojis("{emo:resume} Resuming the print.")
             self.plugin_context.printer.resume_print(user=command_context.user)
+        elif self.plugin_context.printer.is_printing():
+            msg = render_emojis("{emo:pause} Pausing the print.")
+            self.plugin_context.printer.pause_print(user=command_context.user)
         else:
             msg = render_emojis("{emo:warning} Currently I'm not printing, so there is nothing to pause/resume.")
 
